a0cc7da3a85ff77440471bfa7a262c5368a838fc
Sigurd Nes <sigurdne@gmail.com>
2025-08-12 16:04:57 +0200

    fix: Update BRIDGE_URL default value and remove obsolete resource mappings script

--- scripts/enhanced_process_deletions.sh
+++ scripts/enhanced_process_deletions.sh
@@ -6,7 +6,7 @@
 
 set -e
 
-BRIDGE_URL="${BRIDGE_URL:-http://localhost:8082}"
+BRIDGE_URL="${BRIDGE_URL:-http://localhost}"
 LOG_FILE="${LOG_FILE:-/var/log/bridge-deletion-sync.log}"
 TENANT_MODE="${TENANT_MODE:-single}"
 SPECIFIC_TENANT="${1:-}"
